bcd239f69a83a8209309ad61e3c29af18620e56f
tareknagy <hello@tareknagy.com>
2021-09-16 09:09:24 -0400

    add style to typeface component

--- src/components/Typeface/Typeface.jsx
+++ src/components/Typeface/Typeface.jsx
@@ -6,12 +6,10 @@ export default function Typefaces(typeName){
     return (
     <div className="type-card">
         <div className="type-card-pangram" style={{ fontFamily:typeName.typeName }}>{ typeName.typeName }</div>
-        <div className="type-card-decoration">
-            <div className="type-card-name">{ typeName.typeName }</div>
-            <div className="type-card-adjectives">{ typeName.typeName }</div>
-            <div className="type-card-links">ADD TO PROJECTS<br />ADD TO FAVORITES</div>
-            <div className="type-card-describe">DESCRIBE</div>
-        </div>
+        <div className="type-card-name">{ typeName.typeName }</div>
+        <div className="type-card-adjectives">{ typeName.typeName }</div>
+        <div className="type-card-links">ADD TO PROJECTS<br />ADD TO FAVORITES</div>
+        <div className="type-card-describe">DESCRIBE</div>
     </div>
     )
 }
