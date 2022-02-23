e1accdf5924e2630209421821cc4e0b04af2d330
tareknagy <hello@tareknagy.com>
2022-02-24 10:35:55 +1100

    add flag when typeface already in project

--- src/components/AddToProject/AddToProject.jsx
+++ src/components/AddToProject/AddToProject.jsx
@@ -7,60 +7,17 @@ import { useDetectOutsideClick } from '../../utilities/useDetectOutsideClick'
 export default function AddToProject({ typeName, projects, handleCreateProject, recentProjects, handleAddToProject}) {
   const showProjectList = useRef(null);
   const [isActive, setIsActive] = useDetectOutsideClick(showProjectList, false);
-  // const [newProject, setNewProject] = useState('');
-
-  // Handle input changes
-  // async function handleInputChange(e) {
-  //   setNewProject(e.target.value);
-  // }
-
-  // Project List Dropdown
-  function projOnClick(e) {
-    // stop redirect until sublink selected
-    e.preventDefault();
-    setIsActive(!isActive)
-  }
-
-  // check if currently saved to project
-  function isInProject(project) {
-    let found = false;
-    project.typefaces.forEach((t) => {
-      t.replace(/['"]+/g, '') === typeName ? found = true : found = false
-    })
-    return found;
-  }
-
-  // Create new project
-  // function createProject(e)  {
-  //   e.preventDefault();
-  //   // Add New Project
-  //   handleCreateProject(newProject);
-  //   // Clear Input Value
-  //   setNewProject('');
-  // }
-
 
   return (
     <>              
-      {/* <div className='new-project-input'>
-        <form onSubmit={createProject}>
-          <input 
-            type={newProject}
-            placeholder="New Project"
-            value={newProject}
-            onChange={handleInputChange}
-            />
-          <button type="submit">+</button>
-        </form>
-      </div> */}
       <ul>
         {recentProjects && recentProjects.map((project, index) => (
-          <li><Link onClick={() => handleAddToProject(project._id, typeName)}>{project.name}{isInProject(project) ? ' ***' : ''}</Link></li>
+          <li><Link onClick={() => handleAddToProject(project._id, typeName)}>{project.name}{project.typefaces.indexOf(`"${typeName}"`) > -1 ? ' ***' : ''}</Link></li>
         ))}
       </ul>
       <ul>
         {projects && projects.map((project, index) => (
-          <li><Link onClick={() => handleAddToProject(project._id, typeName)}>{project.name}{isInProject(project) ? ' ***' : ''}</Link></li>
+          <li><Link onClick={() => handleAddToProject(project._id, typeName)}>{project.name}{project.typefaces.indexOf(`"${typeName}"`) > -1 ? ' ***' : ''}</Link></li>
         ))}
       </ul>
     </>
